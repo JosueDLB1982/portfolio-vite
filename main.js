e7909c11527cc0484c696e01cc4bb39bbe49a72b
Josue Lopez <josuedlopez1982@gmail.com>
2024-03-05 15:26:42 -0300

    portfolio vite

---
+++ main.js
@@ -0,0 +1,16 @@
+import './style.css'
+import './css/style-button.css'
+
+const downloadBtn = document.querySelector('#download-btn')
+
+downloadBtn.addEventListener('click', () => {
+    setTimeout(() => {
+        const downloadCv = () => {
+            const link = document.createElement('a')
+            link.href = './assets/documents/cv-enero-2024.pdf'
+            link.download = 'Josue Lopez Frontend DEV'
+            link.click()
+        }
+        downloadCv()
+    }, 3000);
+})
