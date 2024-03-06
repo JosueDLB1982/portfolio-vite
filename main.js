fcbd4e040282913405d121e2af33ebb71a772a33
Josue Lopez <josuedlopez1982@gmail.com>
2024-03-06 18:13:04 -0300

    docs: 2.0.1 refactorización del directorio

--- main.js
+++ main.js
@@ -1,17 +1,6 @@
 import './css/normalize.css'
 import './style.css'
 import './css/style-button.css'
+import { App } from './src/app.js'
 
-const downloadBtn = document.querySelector('#download-btn')
-
-downloadBtn.addEventListener('click', () => {
-    setTimeout(() => {
-        const downloadCv = () => {
-            const link = document.createElement('a')
-            link.href = './assets/documents/cv-enero-2024.pdf'
-            link.download = 'Josue Lopez Frontend DEV'
-            link.click()
-        }
-        downloadCv()
-    }, 3000);
-})
+App('#app')
